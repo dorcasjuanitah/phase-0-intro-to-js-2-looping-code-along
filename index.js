7a877977a8667500a007b833c84d3331e17d4d80
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-03-24 11:33:47 +0300

    loops

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-for (let age =30; age < 40; age++){
-    console.log(`I am ${age} years old. Happy Birthday to me!`);
-    debugger;
+const names=['Guadalupe','Ollie','Aki'];
+const event = "surprise";
+
+ 
+ function writeCards( names, event ) {
+  let personalCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+    personalCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
   }
+  return personalCards
+}
+ 
+
+function countDown(){
+  let numCountdown=11;
+  
+  
+  while(numCountdown--){
+    console.log(numCountdown)
+  }
+} 
+
+countDown();
+ 
+
+//for (let age =30; age < 40; age++){
+    //console.log(`I am ${age} years old. Happy Birthday to me!`);
+    //debugger;
+ ///}
+
+  // for loop in arrays
+  
 // Code your solutions in this file
